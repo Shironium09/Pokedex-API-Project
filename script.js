dd76e57a5b3047496789ad8d5ca7532370711f7d
Shironium09 <dustinj.bal09@gmail.com>
2025-04-09 21:43:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const submit = document.getElementById('submit').onclick = () => {
+
+let input = document.getElementById('searchPokemon').value;
+input = input.toUpperCase();
+
+const screen = document.getElementById('screen');
+
+console.log(input);
+
+screen.innerHTML = '';
+
+fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1302')
+.then(response => {
+    if(!response.ok){
+        throw new Error('Failed to fetch data');
+    }
+return response.json();
+})
+.then(pokemon => {
+    pokemon.results.forEach(mon =>{
+        fetch(mon.url)
+        .then(res => res.json())
+        .then(data => {
+
+            let pokemonName = data.name;
+            pokemonName = pokemonName.toUpperCase();
+
+            let flag = false;
+
+            if(pokemonName === input){
+                flag = true;
+            }
+
+            if(flag){
+
+                const name = document.createElement('h1');
+                name.textContent = pokemonName;
+
+                const image = document.createElement('img');
+                image.src = data.sprites.front_default;
+                image.alt = pokemonName;
+
+                screen.appendChild(image);
+                screen.appendChild(name);
+
+            }
+            else{
+
+                const none = document.createElement('h1');
+                none.textContent = 'No Pokemon Found';
+
+            }
+
+        })
+    })
+})
+.catch(error => {
+
+    console.log('Failed to load');
+
+})
+
+}
+
